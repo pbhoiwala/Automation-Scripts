8f0828f7ae49df1e7b39a1d2968983f230f92ae3
parth <parth.v.bhoiwala@gmail.com>
2017-08-28 23:24:05 -0400

    refactored meme bot

--- MemeBot/memebot.py
+++ MemeBot/memebot.py
@@ -4,9 +4,6 @@ from bs4 import BeautifulSoup
 from slacker import Slacker
 
 fileName = "values.txt"
-memes = []
-sentMemes = []
-
 with open(fileName) as f:
     content = f.readlines()
     inputs = [x.strip() for x in content]
@@ -14,44 +11,46 @@ with open(fileName) as f:
     slack_api_token = inputs[1]
     slack_channel = inputs[2]
     slack_memebot_username = inputs[3]
-    print(inputs)
+
+memes = []
+sent_memes = []
 html = urllib2.urlopen(url_to_crawl)
 soup = BeautifulSoup(html, "lxml")
 slack = Slacker(slack_api_token)
 
 
-def sendMemeToSlack(memeImageUrl):
+def send_memes(meme_img_url):
     title = "Check out this meme"
-    textMsg = "Time for some dank memes"
+    text_msg = "Time for some dank memes"
     attch = [{"title": title,
-              "image_url": memeImageUrl}]
-    slack.chat.post_message(slack_channel, textMsg, username=slack_memebot_username, attachments=attch)
+              "image_url": meme_img_url}]
+    slack.chat.post_message(slack_channel, text_msg, username=slack_memebot_username, attachments=attch)
 
 
-def crawl(memeSiteUrl):
-    imgTags = soup.findAll('img')
-    for img in imgTags:
+def crawl():
+    img_tags = soup.findAll('img')
+    for img in img_tags:
         temp = img.get('src')
         if temp[:1] != "/":
-            memeImageUrl = temp
-            #print(memeImageUrl)
-            memes.append(memeImageUrl)
+            meme_image_url = temp
+            memes.append(meme_image_url)
 
 
-def pickAndSendMemes():
-    i=0
-    while(i<3):
-        randomMeme = random.choice(memes)
-        if randomMeme not in sentMemes and 'ifcdn' in randomMeme:
-            sentMemes.append(randomMeme)
-            print(i, randomMeme)
+def pick_memes_to_send():
+    i = 0
+    while i < 3:
+        random_meme = random.choice(memes)
+        if random_meme not in sent_memes and 'ifcdn' in random_meme:
+            print(random_meme)
+            send_memes(random_meme)
+            sent_memes.append(random_meme)
             i += 1
-            sendMemeToSlack(randomMeme)
 
-crawl(url_to_crawl)
-try:
-    pickAndSendMemes()
-    print("Successfully sent memes")
-except:
-    print("Unable to sent memes")
-
+if __name__ == '__main__':
+    crawl()
+    try:
+        pick_memes_to_send()
+        print("Successfully sent memes")
+    except Exception as e:
+        print(str(e))
+        print("Unable to sent memes")
