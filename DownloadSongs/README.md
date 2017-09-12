e2020f616e80fbff14f68d3aa24c9011bce67c0b
parth <parth.v.bhoiwala@gmail.com>
2017-09-12 16:09:06 -0400

    adding readme and refactored some code

---
+++ DownloadSongs/README.md
@@ -0,0 +1,39 @@
+# Download Songs 
+I automated the process of downloading songs from youtube and transferring them to my phone.
+Follow the steps below if you want to set it up:
+First make sure you have Python 2.7 installed and also install the [youtube_dl library](https://github.com/rg3/youtube-dl/blob/master/README.md#readme)
+
+# Configuration
+
+- YouTube playlist
+    - Create a new playlist, call it something like 'Downloads' and add a few songs in it
+    - Go to _Playlist Settings_ and mark the playlist privacy as '_Unlisted_'
+    - '_Unlisted_' means that your playlist is not public but anyone with the link can access it
+    - Copy the url of the playlist from the search bar (remove the extra url parameters like `&disable_polymer=true`
+
+- Google Music manager:
+    - Install [Google Music manager](https://play.google.com/music/listen?u=0#/manager)
+    - Sign into your Google account
+    - Create a new folder somewhere (in your `/home` or `~/Desktop` and call it 'Songs' or whatever you like. Remember the path
+    - Assign that folder to Music Manager for automatic syncing
+    - Install Google Play Music on your smartphone and in _Settings_, enable option to AutoDownload music (on WiFi)
+
+- Script
+    - Download all the files I have in this folder (`main.py`, `logger.py`, `config.txt`, `downloaded_songs.txt`)
+    - Open `config.txt` and set `playlist=` to your youtube playlist url and set `path=` to the path to your Songs folder
+    - Finally, run the main script like this: `python main.py` and if everything is set, you should see output like this
+
+```Parsing the playlist now...
+Song "Not Afraid..." added to cart for download
+Song "Dark Horse..." added to cart for download
+Song "Blank Spac..." added to cart for download
+Downloading songs from the cart...
+Downloading complete, converting now...
+Downloaded 3 new songs
+Moving songs to GooglePlay Library...
+Successfully moved songs to GooglePlay Library 
+```
+
+# How it works
+The `main.py` python script is well commented and the code is pretty self-explanatory. In short, first I initialize some lists and objects; and then parse `config.txt` to get the playlist url and destination path. Then youtube_dl options are created in `get_download_options()` in which I specify the quality and format I'd like the songs to be downloaded.
+Then I parse the playlist, iterate over each song, check if see if song is already downloaded or not, if not, then add the song to a list, and add the song id to `downloaded_songs.txt` and then start the actual download process. The songs get downloaded in current directory, so using `shutil`, the songs are transferred to the 'Songs' folder using the path you provided in config file. Google Music manager takes care of syncing your music from your local computer to your smartphone.
--- DownloadSongs/main.py
+++ DownloadSongs/main.py
@@ -3,12 +3,16 @@ import shutil
 import youtube_dl
 from logger import MyLogger
 
+# Initialize objects
 songs_to_download = []
-list_of_downloaded = []
+list_of_downloaded = set()
 cache_id_file = 'downloaded_songs.txt'
 youtube_base_url = 'https://www.youtube.com/watch?v='
-youtube_playlist = 'your-youtube-playlist-url'
-pathToDestination = 'Path/To/Your/Destination/Directory'
+
+# Read values from user's config file
+config = dict(l.strip().split('=', 1) for l in open('config.txt'))
+youtube_playlist = config['playlist']
+path_to_destination = config['path']
 
 
 def my_hook(d):
@@ -25,7 +29,7 @@ def read_cache_file():
     """
     with open(cache_id_file, 'r') as f:
         global list_of_downloaded
-        return set(f.read().splitlines())
+        list_of_downloaded = set(f.read().splitlines())
 
 
 def get_download_options():
@@ -46,43 +50,54 @@ def get_download_options():
 
 
 def download_songs():
+    """ Parses the playlist and iterates over every song to see if it
+    is already downloaded or not. If not, then adds it to list of songs
+    to  download, adds the song_id to the cache file and starts download
+    """
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         print("Parsing the playlist now...")
         playlist = ydl.extract_info(youtube_playlist, download=False)
         # print(list_of_downloaded)
         for song in playlist['entries']:
-            if not song:
-                print('ERROR: Unable to extract video information')
-                continue
-            song_id, song_title = song.get('id'), song.get('title')
-            if song_id in list_of_downloaded:
-                print('Already downloaded: ' + song_title)
-                continue
-            songs_to_download.append(youtube_base_url + song_id)
-            cache.write(song_id + '\n')
-            print('Song "' + song_title[0:10] + '..." added to cart for download')
-        print('Downloading songs from the cart...')
-        ydl.download(songs_to_download)
+            if song:
+                song_id, song_title = song.get('id'), song.get('title')
+                if song_id in list_of_downloaded:
+                    print('Already downloaded: ' + song_title[0:10] + '...')
+                else:
+                    songs_to_download.append(youtube_base_url + song_id)
+                    cache_file.write(song_id + '\n')
+                    print('Song "' + song_title[0:10] + '..." added to cart for download')
+
+        # make sure there are more than 0 songs to download and transfer them
+        if songs_to_download:
+            print('Downloading songs from the cart...')
+            ydl.download(songs_to_download)
+            print('Downloaded ' + str(len(songs_to_download)) + " new songs")
+            transfer_files()
+        else:
+            print('No new songs to download. Everything is upto date')
 
 
 def transfer_files():
-    print('Downloaded ' + str(len(songs_to_download)) + " new songs")
+    """ Iterates over every 'mp3' file in the current directory and
+    makes sure that the file doesn't exist in the destination
+    directory and then transfer the file
+    """
     print('Moving songs to GooglePlay Library...')
-    try:
-        for file_name in os.listdir("."):
+    for file_name in os.listdir("."):
+        try:
             if file_name.endswith(".mp3"):
-                destination_file = pathToDestination + '/' + file_name
+                destination_file = path_to_destination + '/' + file_name
                 if not os.path.isfile(destination_file):
                     shutil.move(file_name, destination_file)
-        print('Successfully moved songs to GooglePlay Library')
-    except IOError as e:
-        print('ERROR: Unable to move songs to GooglePlay Library')
-        print(str(e.message))
+        except IOError as e:
+            print('ERROR: Unable to move songs to GooglePlay Library')
+            print(str(e.message))
+    print('Successfully moved songs to GooglePlay Library')
 
 
 if __name__ == "__main__":
-    cache = open(cache_id_file, 'a+')
+    cache_file = open(cache_id_file, 'a+')
     ydl_opts = get_download_options()
     read_cache_file()
     download_songs()
-    transfer_files()
